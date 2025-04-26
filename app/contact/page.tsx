f7ef0c9289bc26573cd3fa4f47fab507b465fbaa
Jaydeep <142102780+jaydeepninawe@users.noreply.github.com>
2025-04-26 14:32:13 +0530

    Update now

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -27,7 +27,7 @@ const ContactSection = () => {
     setSubmitStatus(null);
     // const URL = process.env.NEXT_PUBLIC_API_URL;
     try {
-      const response = await fetch(`${URL}/send-email`, {
+      const response = await fetch("https://portfolio-backend-d4mn.onrender.com/send-email", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
